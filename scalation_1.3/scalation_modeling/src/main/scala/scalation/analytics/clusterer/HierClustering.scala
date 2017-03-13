733a34f037e5a8319a684733f38185f2caf09cb8
Nicholas Klepp <MacBot@Nicholass-MacBook-Pro.local>
2017-03-13 19:58:17 -0400

    Implemented multiple reference distributions for gap statistic as well as std dev difference. Implemented complete and average linkage strategies to hierarchical clustering.

--- scalation_1.3/scalation_modeling/src/main/scala/scalation/analytics/clusterer/HierClustering.scala
+++ scalation_1.3/scalation_modeling/src/main/scala/scalation/analytics/clusterer/HierClustering.scala
@@ -14,24 +14,40 @@ import scala.util.control.Breaks.{breakable, break}
 import scalation.linalgebra.{MatrixD, VectorD, VectorI}
 import scalation.util.Error
 
+//::::::
+/** The 'Linkage' object specifies the flavor of Hiererarchical clustering algorithm to use.
+ */
+object Linkage extends Enumeration
+{
+    type Linkage = Value
+    val SINGLE, COMPLETE, AVERAGE = Value
+}// Linkage
+
+import Linkage._
+
 //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 /** Cluster several vectors/points using hierarchical clustering.  Start with
  *  each point forming its own cluster and merge clusters until there are only 'k'.
  *  @param x  the vectors/points to be clustered stored as rows of a matrix
  *  @param k  stop when the number of clusters equals k
  */
-class HierClustering (x: MatrixD, k: Int = 2)
+class HierClustering (x: MatrixD, k: Int = 2, link: Linkage = COMPLETE)
       extends Clusterer with Error
 {
     if (k >= x.dim1) flaw ("constructor", "k must be less than the number of vectors")
 
+    private val DEBUG    = true
     private val cent   = new MatrixD (k, x.dim2)           // the k centroids of clusters
-    private val clustr = Array.ofDim [Int] (x.dim1)        // assignment of vectors to clusters
-    private val clust  = ListBuffer [Set [VectorD]] ()     // the list of clusters
-    private val sizes  = new VectorI (k)
+    private val clustr   = Array.ofDim [Int] (x.dim1)        // assignment of vectors to clusters
+    private val clust    = ListBuffer [Set [VectorD]] ()     // the list of clusters
+    private val sizes    = new VectorI (k)
+    private var dist 	 = Array.ofDim[Array[Double]](x.dim1)
 
+    def clusterDist(a: Set[VectorD],b: Set[VectorD]) = if (link == SINGLE) minClustDist(a,b) else if (link == COMPLETE) maxClustDist(a,b) else avgClustDist(a,b)
+    
     //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-    /** Return the centroids. Should only be called after `cluster ()`. 
+    /** //Return the centroids. Should only be called after `cluster ()`.
+     *  Return the centroids
      */
     def centroids (): MatrixD = cent
 
@@ -48,7 +64,7 @@ class HierClustering (x: MatrixD, k: Int = 2)
     //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
     /** Create initial clusters where each point forms its own cluster.
      *  @param setA  the first set
-     *  @param setB  the second set
+     *  @param setB  the second set 
      */
     def clustDist (setA: Set [VectorD], setB: Set [VectorD]): Double =
     {
@@ -57,12 +73,34 @@ class HierClustering (x: MatrixD, k: Int = 2)
         d
     } // clustDist
 
+    def minClustDist(setA: Set[VectorD], setB: Set [VectorD]): Double =
+    {
+	var d = Double.PositiveInfinity
+	for (a <- setA; b <- setB if distance (a, b) < d) d = distance (a, b)
+	d
+    }
+
+    def maxClustDist(setA: Set[VectorD], setB: Set [VectorD]): Double =
+    {
+	var d = Double.NegativeInfinity
+	for (a <- setA; b <- setB if distance (a, b) > d) d = distance (a, b)
+	d
+    }
+
+    def avgClustDist(setA: Set[VectorD], setB: Set [VectorD]): Double =
+    {
+	var d = 0.0
+	for (a <- setA; b <- setB) d += distance (a, b)
+	d /=  (setA.size * setB.size)
+	d
+    }
+
     //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
     /** Create initial clusters where each point forms its own cluster.
      */
     def initClusters ()
     {
-        for (i <- 0 until x.dim1) clust += Set [VectorD] (x(i))            
+        for (i <- 0 until x.dim1) clust += Set [VectorD] (x(i))
     } // initCluster
 
     //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -77,7 +115,8 @@ class HierClustering (x: MatrixD, k: Int = 2)
         for (kk <- x.dim1 until k by -1) {
             var minDistance = Double.PositiveInfinity
             for (i <- 0 until kk-1; j <- i+1 until kk) {
-                val d_ij = clustDist (clust(i), clust(j))
+                //val d_ij = clustDist (clust(i), clust(j))
+		val d_ij = clusterDist (clust(i), clust(j))
                 if (d_ij < minDistance) {
                     minDistance = d_ij        // update minDistance
                     set_i = clust(i)          // remember point sets i and j
@@ -88,7 +127,7 @@ class HierClustering (x: MatrixD, k: Int = 2)
             clust += (set_i | set_j)          // add the union of sets i and j
             clust -= set_i                    // remove set i
             clust -= set_j                    // remove set j
-            println ("cluster: (" + (kk-1) + ")  clust = " + clust)
+            if (DEBUG) println ("cluster: (" + (kk-1) + ")  clust = " + clust)
         } // for
 
         finalClusters ()                      // make final cluster assignments
@@ -165,12 +204,21 @@ object HierClusteringTest extends App
     println ("z = " + z)
     println ("----------------------------------------------------")
 
-    val cl = new HierClustering (v, 3)                 
+    val cl = new HierClustering (v)                 
     println ("--- final cluster = " + cl.cluster ().deep + "\n")
     println ("--- classify " + y + " = " + cl.classify (y) + "\n")
     println ("--- classify " + z + " = " + cl.classify (z) + "\n")
     println (s"sse = ${cl.sse(v)}")
 
+    for( distMet <- Linkage.values ){
+    	 println(s"Testing $distMet distance metric.")
+	 val cl2 = new HierClustering (v,3)                 
+    	 println ("--- final cluster = " + cl2.cluster ().deep + "\n")
+    	 println ("--- classify " + y + " = " + cl2.classify (y) + "\n")
+    	 println ("--- classify " + z + " = " + cl2.classify (z) + "\n")
+    	 println (s"sse = ${cl2.sse(v)}")
+    }// for
+    
 } // HierClusteringTest object
 
 //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
