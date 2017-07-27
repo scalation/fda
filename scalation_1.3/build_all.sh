8231b37a5098f41f3c7f820c4f127ef26b13b454
Michael E. Cotterell <mepcotterell@gmail.com>
2017-07-27 15:02:36 -0400

    updated build scripts

--- scalation_1.3/build_all.sh
+++ scalation_1.3/build_all.sh
@@ -1,7 +1,6 @@
 
 ####################################################################################################
-# copy_jars.sh - copy jar files from subprojects to lib directories
-#                assumes all subprojects have been compiled and packaged, else use build_all.sh
+# build_all.sh - build all subprojects and copy jar files from subprojects to lib directories
 
 #####################################################################################################
 # subproject dependencies:
@@ -10,18 +9,32 @@
 #     scalation_models   dependes on: scalation_mathstat; scalation_modeling
 
 #####################################################################################################
-# copy the scalation_mathstat subproject's jar file to both dependent subprojects
+# build the scalation_mathstat subproject and copy its jar file to both dependent subprojects
 
+echo ================================= scalation_mathstat ===========================================
+echo // "cd scalation_mathstat; sbt compile package; cd .."
 echo // "cp scalation_mathstat/target/scala-2.12/scalation_mathstat_2.12-1.3.jar scalation_modeling/lib"
 echo // "cp scalation_mathstat/target/scala-2.12/scalation_mathstat_2.12-1.3.jar scalation_models/lib"
 
+cd scalation_mathstat; sbt compile package; cd ..
 cp scalation_mathstat/target/scala-2.12/scalation_mathstat_2.12-1.3.jar scalation_modeling/lib
 cp scalation_mathstat/target/scala-2.12/scalation_mathstat_2.12-1.3.jar scalation_models/lib
 
 #####################################################################################################
-# copy the scalation_modeling subproject's jar file to the dependent subproject
+# build the scalation_modeling subproject and copy its jar file to the dependent subproject
 
+echo ================================= scalation_modeling ===========================================
+echo // "cd scalation_modeling; sbt compile package; cd .."
 echo // "cp scalation_modeling/target/scala-2.12/scalation_modeling_2.12-1.3.jar scalation_models/lib"
 
+cd scalation_modeling; sbt compile package; cd ..
 cp scalation_modeling/target/scala-2.12/scalation_modeling_2.12-1.3.jar scalation_models/lib
+
+#####################################################################################################
+# build the scalation_models subproject
+
+echo ================================== scalation_models ============================================
+echo // "cd scalation_models compile; cd .."
+
+cd scalation_models compile; cd ..
 
